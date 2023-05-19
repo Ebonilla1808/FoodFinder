84e8234f58d2aa32d9afcceeed30ba93abdd9df2
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-05-19 11:18:16 -0600

    mejoras de diseño

--- app/src/main/java/sv/edu/utec/foodfinder/MainActivity.java
+++ app/src/main/java/sv/edu/utec/foodfinder/MainActivity.java
@@ -2,14 +2,25 @@ package sv.edu.utec.foodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnStart);
 
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
 }
