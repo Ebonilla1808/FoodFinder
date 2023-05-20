6dfb797d502e5a383e082097771a4a17b877653b
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-05-19 22:13:00 -0600

    Refactor a activity

--- app/src/main/java/sv/edu/utec/foodfinder/RestauranteContactoActivity.java
+++ app/src/main/java/sv/edu/utec/foodfinder/RestauranteContactoActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RestauranteContacto extends AppCompatActivity {
+public class RestauranteContactoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
