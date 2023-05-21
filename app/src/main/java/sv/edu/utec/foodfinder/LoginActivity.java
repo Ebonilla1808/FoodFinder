256a00f51b0adb7e2bb6b272f821bc0422737c1d
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-05-21 14:04:34 -0600

    agregando pantalla de registro de usuario

--- app/src/main/java/sv/edu/utec/foodfinder/LoginActivity.java
+++ app/src/main/java/sv/edu/utec/foodfinder/LoginActivity.java
@@ -6,20 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
+    TextView btnCrear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnLogin = findViewById(R.id.btnLogin);
+        btnCrear = findViewById(R.id.tvRegistrer);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            }
+        });
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateUserActivity.class));
             }
         });
     }
---
+++ app/src/main/java/sv/edu/utec/foodfinder/entidades/EntUsuarios.java
@@ -0,0 +1,76 @@
+package sv.edu.utec.foodfinder.entidades;
+
+public class EntUsuarios {
+    private int id_usuario;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String telefono;
+    private String nombre_usuario;
+    private String contrasenia;
+    private int tipo_usuario;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public int getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    public void setTipo_usuario(int tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+}
