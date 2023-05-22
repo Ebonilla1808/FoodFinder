bb4be9db2c2b34dd9ffb8b6b27703bd6854d34f1
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-05-22 16:17:14 -0600

    Creando metodos para obtener lista de restaurantes por categorias

--- app/src/main/java/sv/edu/utec/foodfinder/HomeActivity.java
+++ app/src/main/java/sv/edu/utec/foodfinder/HomeActivity.java
@@ -12,19 +12,64 @@ import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ImageView btnCafe;
+    ImageView btnCafe, btnTipico, btnMarisco, btnRapida, btnBuffets, btnHeladerias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         btnCafe = findViewById(R.id.image_coffe);
+        btnTipico = findViewById(R.id.image_tipico);
+        btnMarisco = findViewById(R.id.image_mariscos);
+        btnRapida = findViewById(R.id.image_fast);
+        btnBuffets = findViewById(R.id.image_buffets);
+        btnHeladerias = findViewById(R.id.image_heladerias);
 
         btnCafe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ListRestaurantesActivity.class);
                 intent.putExtra("idCategoria", 1);
+                startActivity(intent);
+            }
+        });
+        btnTipico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListRestaurantesActivity.class);
+                intent.putExtra("idCategoria", 2);
+                startActivity(intent);
+            }
+        });
+        btnMarisco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListRestaurantesActivity.class);
+                intent.putExtra("idCategoria", 3);
+                startActivity(intent);
+            }
+        });
+        btnRapida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListRestaurantesActivity.class);
+                intent.putExtra("idCategoria", 4);
+                startActivity(intent);
+            }
+        });
+        btnBuffets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListRestaurantesActivity.class);
+                intent.putExtra("idCategoria", 5);
+                startActivity(intent);
+            }
+        });
+        btnHeladerias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListRestaurantesActivity.class);
+                intent.putExtra("idCategoria", 6);
                 startActivity(intent);
             }
         });
--- app/src/main/java/sv/edu/utec/foodfinder/ListRestaurantesActivity.java
+++ app/src/main/java/sv/edu/utec/foodfinder/ListRestaurantesActivity.java
@@ -1,20 +1,37 @@
 package sv.edu.utec.foodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 
-public class ListRestaurantesActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import sv.edu.utec.foodfinder.ViewModel.RestaurantesViewModel;
+import sv.edu.utec.foodfinder.adaptadores.ListRestaurantesAdapter;
+import sv.edu.utec.foodfinder.datos.Restaurantes;
+
+public class ListRestaurantesActivity extends AppCompatActivity {
+    RecyclerView listRestaurantes;
+    ArrayList<RestaurantesViewModel> AlistRestaurantes;
+  //  Context context;
      int idCategoria;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_restaurantes);
         idCategoria = getIntent().getIntExtra("idCategoria", 0);
+        listRestaurantes = findViewById(R.id.rvwListaRestaurantes);
 
         if(idCategoria!=0){
+            listRestaurantes.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            Restaurantes dbRestaurantes = new Restaurantes(getApplicationContext());
+            AlistRestaurantes=new ArrayList<>();
 
+            ListRestaurantesAdapter adapter=new ListRestaurantesAdapter(dbRestaurantes.obtenerListaRestaurantes(idCategoria));
+            listRestaurantes.setAdapter(adapter);
         }
     }
 }
