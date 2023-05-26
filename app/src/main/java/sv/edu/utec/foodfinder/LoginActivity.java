3d22b9d08fd755a82ad34f589e83a2810c82f938
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-05-26 10:06:52 -0600

    recordar credenciales funcional

--- app/src/main/java/sv/edu/utec/foodfinder/LoginActivity.java
+++ app/src/main/java/sv/edu/utec/foodfinder/LoginActivity.java
@@ -1,11 +1,14 @@
 package sv.edu.utec.foodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +21,12 @@ public class LoginActivity extends AppCompatActivity {
     TextView btnCrear;
     EditText edtUser, edtPass;
     Usuarios usuarios;
+
+
+
+    CheckBox chkRememberMe;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,17 @@ public class LoginActivity extends AppCompatActivity {
         btnCrear = findViewById(R.id.tvRegistrer);
         edtUser = findViewById(R.id.edtUser);
         edtPass = findViewById(R.id.edtPss);
+        chkRememberMe = findViewById(R.id.chkRememberMe);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean rememberMe = sharedPreferences.getBoolean("rememberMe", false);
+        if (rememberMe) {
+            String savedUsername = sharedPreferences.getString("username", "");
+            String savedPassword = sharedPreferences.getString("password", "");
+            edtUser.setText(savedUsername);
+            edtPass.setText(savedPassword);
+            chkRememberMe.setChecked(true);
+        }
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +53,29 @@ public class LoginActivity extends AppCompatActivity {
                 String usuario = edtUser.getText().toString();
                 String password = edtPass.getText().toString();
                 usuarios = new Usuarios(getApplicationContext());
+
+
+
                 if (usuarios.iniciarSesion(usuario, password)){
+
+
+                    if (chkRememberMe.isChecked()) {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putBoolean("rememberMe", true);
+                        editor.putString("username", usuario);
+                        editor.putString("password", password);
+                        editor.apply();
+                    } else {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putBoolean("rememberMe", false);
+                        editor.putString("username", "");
+                        editor.putString("password", "");
+                        editor.apply();
+                    }
+
+
+
+
                     startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                     Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_SHORT).show();
                 }
